29f6968168b266cb111e11d30ebf211a198b20f9
Hadi Alsinan <dev.hadialsinan@gmail.com>
2024-03-09 01:18:17 +0300

    Add dropdown menu for leaderboard

--- lib/HomePage/Screens/LeaderBoard.dart
+++ lib/HomePage/Screens/LeaderBoard.dart
@@ -1,6 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+final List<Map<String, dynamic>> items = [
+  {
+    'name': 'فيصل العتيبي',
+    'bookname': 'العادات الذرية',
+    'pagenumber': '1',
+    'quote': "النجاح ليس نهاية الطريق بل بداية للمزيد من النجاح",
+    'isLiked': false,
+  },
+  {
+    'name': 'إيمان الشهري',
+    'bookname': 'الشحرورة الذهبية',
+    'pagenumber': '35',
+    'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
+    'isLiked': false,
+  },
+  {
+    'name': 'يوسف أحمد',
+    'bookname': 'أمريكانا',
+    'pagenumber': '78',
+    'quote':
+        "إذا لم تفهم، اسأل. وإذا كنت تشعر بالضيق من الأسئلة، فقل أنك كذلك ثم اسأل على أي حال",
+    'isLiked': false,
+  },
+  {
+    'name': 'سارة عبدالله',
+    'bookname': 'كبرياء وتحامل',
+    'pagenumber': '5',
+    'quote':
+        "يُعترف عالميًا بأن الرجل الأعزب المالك لثروة طيبة، لابد أن يكون بحاجة لزوجة",
+    'isLiked': false,
+  },
+  {
+    'name': 'محمد البقمي',
+    'bookname': 'الخيميائي',
+    'pagenumber': '22',
+    'quote': "وعندما تريد شيئًا، يتآمر الكون كله لمساعدتك على تحقيقه",
+    'isLiked': false,
+  },
+  {
+    'name': 'أمل العتيبي',
+    'bookname': 'الشحرورة الذهبية',
+    'pagenumber': '35',
+    'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
+    'isLiked': false,
+  },
+  {
+    'name': 'عبدالله الشهراني',
+    'bookname': 'أمريكانا',
+    'pagenumber': '78',
+    'quote':
+        "إذا لم تفهم، اسأل. وإذا كنت تشعر بالضيق من الأسئلة، فقل أنك كذلك ثم اسأل على أي حال",
+    'isLiked': false,
+  },
+  {
+    'name': 'نورة العتيبي',
+    'bookname': 'كبرياء وتحامل',
+    'pagenumber': '5',
+    'quote':
+        "يُعترف عالميًا بأن الرجل الأعزب المالك لثروة طيبة، لابد أن يكون بحاجة لزوجة",
+    'isLiked': false,
+  },
+  {
+    'name': 'عبدالله البقمي',
+    'bookname': 'الخيميائي',
+    'pagenumber': '22',
+    'quote': "وعندما تريد شيئًا، يتآمر الكون كله لمساعدتك على تحقيقه",
+    'isLiked': false,
+  },
+  {
+    'name': 'فاطمة العتيبي',
+    'bookname': 'الشحرورة الذهبية',
+    'pagenumber': '35',
+    'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
+    'isLiked': false,
+  },
+];
+
 class LeaderBoardScreen extends StatefulWidget {
   const LeaderBoardScreen({Key? key}) : super(key: key);
 
@@ -9,86 +86,18 @@ class LeaderBoardScreen extends StatefulWidget {
 }
 
 class _LeaderBoardScreenState extends State<LeaderBoardScreen> {
-  final List<Map<String, dynamic>> items = [
-    {
-      'name': 'فيصل العتيبي',
-      'bookname': 'العادات الذرية',
-      'pagenumber': '1',
-      'quote': "النجاح ليس نهاية الطريق بل بداية للمزيد من النجاح",
-      'isLiked': false,
-    },
-    {
-      'name': 'إيمان الشهري',
-      'bookname': 'الشحرورة الذهبية',
-      'pagenumber': '35',
-      'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
-      'isLiked': false,
-    },
-    {
-      'name': 'يوسف أحمد',
-      'bookname': 'أمريكانا',
-      'pagenumber': '78',
-      'quote':
-          "إذا لم تفهم، اسأل. وإذا كنت تشعر بالضيق من الأسئلة، فقل أنك كذلك ثم اسأل على أي حال",
-      'isLiked': false,
-    },
-    {
-      'name': 'سارة عبدالله',
-      'bookname': 'كبرياء وتحامل',
-      'pagenumber': '5',
-      'quote':
-          "يُعترف عالميًا بأن الرجل الأعزب المالك لثروة طيبة، لابد أن يكون بحاجة لزوجة",
-      'isLiked': false,
-    },
-    {
-      'name': 'محمد البقمي',
-      'bookname': 'الخيميائي',
-      'pagenumber': '22',
-      'quote': "وعندما تريد شيئًا، يتآمر الكون كله لمساعدتك على تحقيقه",
-      'isLiked': false,
-    },
-    {
-      'name': 'أمل العتيبي',
-      'bookname': 'الشحرورة الذهبية',
-      'pagenumber': '35',
-      'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
-      'isLiked': false,
-    },
-    {
-      'name': 'عبدالله الشهراني',
-      'bookname': 'أمريكانا',
-      'pagenumber': '78',
-      'quote':
-          "إذا لم تفهم، اسأل. وإذا كنت تشعر بالضيق من الأسئلة، فقل أنك كذلك ثم اسأل على أي حال",
-      'isLiked': false,
-    },
-    {
-      'name': 'نورة العتيبي',
-      'bookname': 'كبرياء وتحامل',
-      'pagenumber': '5',
-      'quote':
-          "يُعترف عالميًا بأن الرجل الأعزب المالك لثروة طيبة، لابد أن يكون بحاجة لزوجة",
-      'isLiked': false,
-    },
-    {
-      'name': 'عبدالله البقمي',
-      'bookname': 'الخيميائي',
-      'pagenumber': '22',
-      'quote': "وعندما تريد شيئًا، يتآمر الكون كله لمساعدتك على تحقيقه",
-      'isLiked': false,
-    },
-    {
-      'name': 'فاطمة العتيبي',
-      'bookname': 'الشحرورة الذهبية',
-      'pagenumber': '35',
-      'quote': "الضحك كان نورًا، والنور كان ضحكًا، وكان هذا سر الكون",
-      'isLiked': false,
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.1,
+          ),
+          child: DropdownLeaders(),
+        ),
+        preferredSize: Size.fromHeight(100.0),
+      ),
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
@@ -145,7 +154,7 @@ class _LeaderBoardScreenState extends State<LeaderBoardScreen> {
                         ],
                       ),
                       const SizedBox(height: 8),
-                      Icon(
+                      const Icon(
                         Icons.format_quote,
                         color: Colors.green,
                       ),
@@ -174,6 +183,79 @@ class _LeaderBoardScreenState extends State<LeaderBoardScreen> {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class DropdownLeaders extends StatefulWidget {
+  const DropdownLeaders({super.key});
+
+  @override
+  _DropdownLeadersState createState() => _DropdownLeadersState();
+}
+
+class _DropdownLeadersState extends State<DropdownLeaders> {
+  Map<String, dynamic>? selectedLeader;
+
+  @override
+  Widget build(BuildContext context) {
+    // Sort items based on the total number of pages read in descending order
+    items.sort((a, b) =>
+        int.parse(b['pagenumber']).compareTo(int.parse(a['pagenumber'])));
+
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: DropdownButton<Map<String, dynamic>>(
+          dropdownColor: Colors.green, // Set the dropdown color to green
+          focusColor: Colors.green,
+          value: selectedLeader,
+          isExpanded: true,
+          icon: Icon(
+            Icons.arrow_drop_down,
+            color: Colors.white,
+          ),
+          hint: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.leaderboard_outlined,
+                color: Colors.white,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                'المتصدرين',
+                style: GoogleFonts.readexPro(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          borderRadius: BorderRadius.circular(20.0),
+          style: GoogleFonts.readexPro(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          underline: Container(), // Remove the underline
+          onChanged: (Map<String, dynamic>? newValue) {},
+          items: items.map<DropdownMenuItem<Map<String, dynamic>>>(
+              (Map<String, dynamic> leader) {
+            return DropdownMenuItem<Map<String, dynamic>>(
+              value: leader,
+              child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Text(
+                      '${leader['name']} - ${leader['pagenumber']} اقتباس')),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
